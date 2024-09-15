c8663a4092c4723ffc297bb68f2436c78b57490a
HuzaifaInam5 <huzaifainam57@gmail.com>
2024-09-15 23:06:16 +0500

    Update images.py

--- Files/images.py
+++ Files/images.py
@@ -1,1 +1,16 @@
+import cv2
+
+path = '../Images/image_1.jpg'
+image = cv2.imread(path)
+
+# Check if the image was successfully loaded
+if image is None:
+    print("Error loading image")
+else:
+    # Display the image in a window
+    cv2.imshow('Loaded Image', image)
+
+    # Wait for a key press and close the window
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
